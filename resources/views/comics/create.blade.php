a871707e6b4f89ee70ccbcd185ebe85890b60971
Aleandro2119 <aleandro.basilico@gmail.com>
2022-04-06 00:13:14 +0200

    Add validatio

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,39 +2,48 @@
 
 @section('content')
     <div class="container">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
         <form action="{{ route('comics.store') }}" method="post">
             @csrf
 
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Title</span>
-                <input type="text" class="form-control" placeholder="Enter Title" name="title" required>
+                <input type="text" class="form-control" placeholder="Enter Title" value="{{ old('title') }}" name="title" required>
                 <span class="input-group-text" id="basic-addon1">Series</span>
-                <input type="text" class="form-control" placeholder="Enter Series" name="series" required>
+                <input type="text" class="form-control" placeholder="Enter Series" value="{{ old('series') }}"  name="series" required>
             </div>
 
             <div class="input-group mb-3">
-                <input type="date" class="form-control" placeholder="Sale Date" name="sale_date" required>
+                <input type="date" class="form-control" placeholder="Sale Date" name="sale_date"  value="{{ old('sale_date') }}" required>
             </div>
 
 
             <div class="input-group mb-3">
                 <span class="input-group-text"></span>
-                <input type="text" class="form-control" placeholder="Comic image URL" name="thumb" required>
+                <input type="text" class="form-control" placeholder="Comic image URL" value="{{ old('thumb') }}"  name="thumb" required>
             </div>
 
             <div class="input-group mb-3">
                 <span class="input-group-text">€</span>
-                <input type="number" class="form-control" default="0" min="0" placeholder="Price" name="price" required>
+                <input type="number" class="form-control" default="0" min="0" placeholder="Price" value="{{ old('price') }}"  name="price" required>
                 <span class="input-group-text">.00</span>
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Type" name="type" required>
+                <input type="text" class="form-control" placeholder="Type" value="{{ old('type') }}"  name="type" required>
             </div>
 
             <div class="input-group">
                 <span class="input-group-text">Decription</span>
-                <textarea class="form-control" name="description" required></textarea>
+                <textarea class="form-control" name="description" value="{{ old('description') }}"  required></textarea>
             </div>
 
             <button class="btn btn-danger mt-3" type="reset">Reset</button>
